486e46b402ce675c228271a1855fc4b399733b6a
Larmbs <liamisrael200@gmail.com>
2024-08-06 20:17:17 -0400

    .

--- main.js
+++ main.js
@@ -7,99 +7,118 @@ const colWidth = 90;
 const circleRadius = 40;
 
 // Function to calculate factorial with memoization
-const factorial = (function() {
-    const cache = {};
-    return function(n) {
-        if (n < 0) return 0;
-        if (n === 0 || n === 1) return 1;
-        if (cache[n]) return cache[n];
-        return (cache[n] = n * arguments.callee(n - 1));
-    };
+const factorial = (function () {
+  const cache = {};
+  return function (n) {
+    if (n < 0) return 0;
+    if (n === 0 || n === 1) return 1;
+    if (cache[n]) return cache[n];
+    return (cache[n] = n * arguments.callee(n - 1));
+  };
 })();
 
 // Function to calculate binomial coefficient C(i, j) = i! / (j! * (i - j)!)
 function pascalValue(i, j) {
-    if (j > i || i < 0 || j < 0) {
+  if (i >= 0) {
+    if (j <= i && j >= 0) {
+      return Math.floor(factorial(i) / (factorial(j) * factorial(i - j)));
+    } else {
+      return 0;
+    }
+  } else {
+    if (i < j && j + i % 2>= 0) {
+      let offset_i = -2 * Math.round(i / 2);
+      let sign;
+      if ((i + (j % 2) - 1) % 2 == 0) {
+        sign = -1;
+      } else {
+        sign = 1;
+      }
+      return Math.floor(
+        (sign * factorial(j - 1 + offset_i)) /
+          (factorial(-i - 1) * factorial(j + offset_i + i))
+      );
+    } else {
         return 0;
     }
-    return factorial(i) / (factorial(j) * factorial(i - j));
+  }
 }
 
 function get_color(integer) {
-    const colors = [
-        "rgb(74, 99, 255)",
-        "rgb(255, 240, 74)",
-        "red",
-        "grey",
-        "green",
-        "purple",
-        "orange",
-        "pink",
-    ];
-    if (integer == 0) {
-        return "black";
-    } else {
-        return colors[Math.abs(integer) % colors.length];
-    }    
+  const colors = [
+    "rgb(74, 99, 255)",
+    "rgb(255, 240, 74)",
+    "red",
+    "grey",
+    "green",
+    "purple",
+    "orange",
+    "pink",
+  ];
+  if (integer == 0) {
+    return "black";
+  } else {
+    return colors[Math.floor(Math.abs(integer) % colors.length)];
+  }
 }
 
 function draw() {
-    ctx.strokeStyle = "white";
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.lineWidth = 4;
-    ctx.fillStyle = "black";
-    ctx.font = "20px Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.lineWidth = 4;
+  ctx.fillStyle = "black";
+  ctx.font = "20px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
 
-    const visibleRows = Math.ceil(canvas.height / rowHeight) + 2;
-    const visibleCols = Math.ceil(canvas.width / colWidth) + 2;
-    let offset_X = -Math.round(camera_pos[0] / colWidth) - 1;
-    let offset_Y = -Math.round(camera_pos[1] / rowHeight) - 1 ;
+  const visibleRows = Math.ceil(canvas.height / rowHeight) + 2;
+  const visibleCols = Math.ceil(canvas.width / colWidth) + 2;
+  let offset_X = -Math.round(camera_pos[0] / colWidth) - 1;
+  let offset_Y = -Math.round(camera_pos[1] / rowHeight) - 1;
 
-    for (let i = offset_Y; i < visibleRows + offset_Y; i++) {
-        for (let j = offset_X; j <= visibleCols + offset_X; j++) {
+  for (let i = offset_Y; i < visibleRows + offset_Y; i++) {
+    for (let j = offset_X; j <= visibleCols + offset_X; j++) {
+      let val = pascalValue(i, Math.round(j + i / 2));
+      if (val != 0) {
+        let x = camera_pos[0] + (j + (i % 2) / 2) * colWidth;
+        let y = camera_pos[1] + i * rowHeight;
 
-            let val = pascalValue(i, Math.round(j + i / 2));
+        ctx.fillStyle = get_color(val);
+        ctx.beginPath();
+        ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
 
-            let x = camera_pos[0] + (j + (i % 2) / 2) * colWidth;
-            let y = camera_pos[1] + i * rowHeight;
-
-            ctx.fillStyle = get_color(val);
-            ctx.beginPath();
-            ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.stroke();
-
-            ctx.fillStyle = "black";
-            ctx.fillText(val, x, y);
-        }
+        ctx.fillStyle = "black";
+        ctx.fillText(val, x, y);
+      }
     }
+  }
 }
 
 window.addEventListener("keydown", check, false);
 
 function check(e) {
-    var code = e.keyCode;
-    switch (code) {
-        case 37: // Left arrow key
-            camera_pos[0] += 10;
-            draw();
-            break;
-        case 38: // Up arrow key
-            camera_pos[1] += 10;
-            draw();
-            break;
-        case 39: // Right arrow key
-            camera_pos[0] -= 10;
-            draw();
-            break;
-        case 40: // Down arrow key
-            camera_pos[1] -= 10;
-            draw();
-            break;
-    }
+  var code = e.keyCode;
+  switch (code) {
+    case 37: // Left arrow key
+      camera_pos[0] += 10;
+      draw();
+      break;
+    case 38: // Up arrow key
+      camera_pos[1] += 10;
+      draw();
+      break;
+    case 39: // Right arrow key
+      camera_pos[0] -= 10;
+      draw();
+      break;
+    case 40: // Down arrow key
+      camera_pos[1] -= 10;
+      draw();
+      break;
+  }
 }
 
 draw();
